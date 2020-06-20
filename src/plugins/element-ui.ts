ce4dead746030187d1a4aa91834d3c1989916613
Nikita Kuprin <nikon5070@gmail.com>
2020-06-20 14:45:49 +1000

    (add) element ui

---
+++ src/plugins/element-ui.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import { Button, Input } from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(Input);
+Vue.use(Button);
+
+declare module 'element-ui';
